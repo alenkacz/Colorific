a95e89d708f5c751bbb2eac19bcd05e66ae9e4b4
alenkacz <varkockova.a@gmail.com>
2011-11-12 21:13:44 +0100

    onclick and change the color of the tile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var canvas;
+var gameDesk;
 
 $(document).ready(function() {
 	canvas = document.getElementById('main-surface');
 	
-	var gameDesk = new GameDesk();
+	gameDesk = new GameDesk();
+	gameDesk.initDesk();
 	gameDesk.drawDesk();
 	$(document).click(gameDesk.handleClick);
 });
@@ -12,31 +14,79 @@ $(document).ready(function() {
 
 function GameDesk() {
 	this.tileSize = 50;
+	this.spaceSize = 2;
 	this.deskSize = 10;
 	this.startPos = 50;
 	this.desk = new Array(this.deskSize);
 	
-	for(var i=0; i <3; i++) {
+	for(var i=0; i <this.deskSize; i++) {
 		this.desk[i] = new Array(this.deskSize);
 	}
 }
 
-GameDesk.prototype.drawDesk = function() {
+GameDesk.prototype.initDesk = function() {
 	var x = this.startPos, y = this.startPos;
 	for(var i = 0; i < this.deskSize; i++) {
 		for(var j = 0; j < this.deskSize; j++) {
-			this.desk[i,j] = new Rectangle(x,y, this.tileSize);
-			this.desk[i,j].draw();
-			
-			x += this.tileSize + 2;
+			this.desk[i][j] = new Rectangle(x,y, this.tileSize);
+			x += this.tileSize + this.spaceSize;
 		}
-		y += this.tileSize + 2;
+		y += this.tileSize + this.spaceSize;
 		x = this.startPos;
 	}
 }
 
+GameDesk.prototype.drawDesk = function() {
+	for(var i = 0; i < this.deskSize; i++) {
+		for(var j = 0; j < this.deskSize; j++) {
+			this.desk[i][j].draw();
+		}
+	}
+}
+
+GameDesk.prototype.repaint = function() {
+	var ctx = canvas.getContext('2d');
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	gameDesk.drawDesk();
+}
+
+/**
+ * Determines whether the given x,y position is inside the gamedesk
+ */
+GameDesk.prototype.isInsideDesk = function(x,y) {
+	if(x < this.startPos || y < this.startPos) {
+		return false;
+	}
+	var deskSize = this.startPos + this.deskSize*(this.tileSize+this.spaceSize);
+	if((x < deskSize) && (y < deskSize)) {
+		return true;
+	}
+	
+	return false;
+}
+
+/**
+ * Returns position of the tile inside the gamedesk
+ * @param x x position of the click
+ * @returns {Number} position of the tile
+ */
+GameDesk.prototype.getTilePosition = function(x) {
+	var pos = x - this.startPos;
+	return parseInt(pos / (this.tileSize + this.spaceSize/2));
+}
+
+GameDesk.prototype.handleTileClick = function(row,side) {
+	this.desk[row][side].color = "ff0000";
+	gameDesk.repaint();
+}
+
 GameDesk.prototype.handleClick = function(e) {
-	// TODO
+	if(gameDesk.isInsideDesk(e.clientX, e.clientY)) {
+		var side = gameDesk.getTilePosition(e.clientX);
+		var row = gameDesk.getTilePosition(e.clientY);
+		
+		gameDesk.handleTileClick(row,side);
+	}
 }
 
 /** RECTANGLE **/
@@ -45,9 +95,11 @@ function Rectangle(x,y, tileSize) {
 	this.x = x;
 	this.y = y;
 	this.tileSize = tileSize;
+	this.color = "#000";
 }
 
 Rectangle.prototype.draw = function() {
 	var ctx = canvas.getContext('2d'); 
+	ctx.fillStyle = this.color;
 	ctx.fillRect(this.x,this.y,this.tileSize,this.tileSize);
 }
