ba53ecb9003b76c76926142b90dfbb4e0cb74442
alenkacz <varkockova.a@gmail.com>
2011-12-02 13:56:37 +0100

    Seconds part of the timer fixed

--- js/timer.js
+++ js/timer.js
@@ -7,11 +7,12 @@ Timer.prototype.init = function() {
 	this.timer = window.setInterval(timer.updateTime, 500);
 };
 
+/**
+ * Updates displayed time, called as a setInterval callback
+ */
 Timer.prototype.updateTime = function() {
 	var now = new Date();
 	var diff = now - timer.startTime;
-	
-	console.log(diff);
 	
 	var minutes = timer.getMinutes(diff);
 	var seconds = timer.getSeconds(diff);
@@ -21,10 +22,20 @@ Timer.prototype.updateTime = function() {
 	$("#timer").html(timeString);
 };
 
+/**
+ * Gets number of minutes from miliseconds
+ * @param diff number of miliseconds from game start
+ * @returns number of minutes from game start
+ */
 Timer.prototype.getMinutes = function(diff) {
 	return parseInt(diff / 60000);
 };
 
+/**
+ * Get number of seconds from miliseconds
+ * @param diff number of miliseconds from the game start
+ * @returns {Number} number of seconds from the game last minutes
+ */
 Timer.prototype.getSeconds = function(diff) {
-	return parseInt(diff / 1000);
+	return (parseInt(diff / 1000) % 60);
 };
